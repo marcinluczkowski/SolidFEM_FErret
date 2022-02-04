fbf97af9b72a20117d285be3e77e2f474d730e97
Markus Aleksander Wulff <markusaw@ntnu.no>
2022-02-04 08:36:22 +0100

    Laget ny klasse

---
+++ SolidFEM/Classes/testClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidFEM.Classes
+{
+    class testClass
+    {
+        public string Name { get; }
+        public int ID { get; set; }
+
+        public testClass()
+        {
+
+        }
+    }
+}
