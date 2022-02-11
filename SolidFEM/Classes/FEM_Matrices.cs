3a5d30e3296b009fa9bba1c5d83d9e00a28411ad
Sverre Magnus Haakonsen <sverremh@ntnu.no>
2022-02-11 12:47:59 +0100

    Ryddet noe  i mapper.

---
+++ SolidFEM/Classes/FEM_Matrices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidFEM.Classes
+{
+    /// <summary>
+    /// A class for all the matrix methods needed in the finite element analysis. 
+    /// </summary>
+    static class FEM_Matrices
+    {
+        
+        // To do: Move all the methods working on matrices in the component to this class. Need to create a logger first to keep track of the information.
+    }
+}
